9736f7d907846da706be7fd482c319632a962214
enikov1 <73391044+enikov1@users.noreply.github.com>
2021-03-05 14:48:35 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 $(document).ready(function () {
-	new WOW().init();
 
-	
+	//Mob menu close 
+  $('.mobile-mnu .mob-line .mnu-btn').click(function(){
+    $(this).toggleClass('hide');
+    $('.mobile-mnu .sidebar').toggleClass('active');
+  });
+  $(".mobile-mnu li a").click(function() {
+     $(".mob-btn").trigger("click");
+  });
 });
